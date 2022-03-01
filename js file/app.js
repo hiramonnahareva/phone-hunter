f9d7ff8f9e0d59056f37187710c3313f7594b176
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-03-01 13:39:23 +0600

    add details info

--- js file/app.js
+++ js file/app.js
@@ -1,20 +1,41 @@
+// error function 
+const errorFunction = (id , displayStyle) => {
+    const error = document.getElementById (id) ;
+     error.style.display = displayStyle
+}
+
 // searching input 
 const loadingButton = () => {
     const input = document.getElementById ('input') ;
     const searchText = input.value ;
+   if (searchText == ''){
+    //    console.log ('error')
+      errorFunction ('error','block') ;
+      errorFunction('all-display' , 'none') ;
+
+   }
+   else {
+       errorFunction('error','none') ;
     input.value = '' ;
     // console.log (searchResultvalue) ;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch (url) 
     .then (res => res.json ())
-    .then (data => display (data))
+    .then (products => display (products.data))
+   }
 
 }
-const display = phone => {
+const display = products => {
     const displayDiv = document.getElementById ('display')
+    if (products == ''){
+        console.log ('hdhds')
+         errorFunction ('error','block') ;
+      errorFunction('all-display' , 'none') ;
+    }
+    else{
     displayDiv.textContent = '' ;
-    const phoneData = phone.data ;
-    phoneData.forEach(element => {
+    const products20 = products.slice (0 , 12) ;
+    products20.forEach(element => {
         // console.log (element.slug) ;
       const div =  document.createElement ('div') ;
       div.className= 'col-md-4 my-2'
@@ -26,8 +47,9 @@ const display = phone => {
       <button onclick="detailsBtn('${element.slug}')" class='btn btn-outline-dark mt-2'>Detail</button>
       </div>
       `
-      displayDiv.appendChild (div)
+      displayDiv.appendChild (div) ;
     });
+}
 }
 // details button function
 const detailsBtn = id => {
@@ -40,6 +62,30 @@ const detailsBtn = id => {
 // details information display function
 const detailInfo = (details) => {
     const displayDetails = document.getElementById ('details')
+    const others = details.data.others
+    // console.log (details.data)
+    const {Bluetooth , GPS , NFC ,Radio , WLAN} = others
+    const sensors = (details.data.mainFeatures.sensors)
+    const [FaceID , accelerometer , gyro , proximity , compass , barometer] = sensors ;
+    console.log (sensors)
+    const releaseDate = (details.data.releaseDate)
+    if (releaseDate == ''){
+        displayDetails.innerHTML = `<div class='p-5'>
+        <div class ='d-flex justify-content-center'> 
+        <img src='${details.data.image}' >
+        </div>
+        <h1 class='text-center'>${details.data.name}</h1>
+        <h4 class='text-center'>${details.data.brand}</h4>
+        <h5 class='text-center'>${details.data.releaseDate}</h5>
+        <h5 class='text-center text-danger'> Release date is no found</h5>
+        <h6 class='text-center mt-5'><span class='mx-3'>Bluetooth: ${Bluetooth}</span><span class='ms-3'>Gps: ${GPS}</span></h5>
+        <h6 class='text-center my-3'><span class='ms-5'>NFC: ${NFC}</span> <span class='ms-5'>Radio: ${Radio}</span><span class='ms-5'>Radio: ${USB}</span></h6>
+        <h6 class='text-center'>WLAN: ${WLAN}</span></h6>
+        <h6 class='text-center'>Sensors : ${FaceID} , ${accelerometer} , ${gyro} , ${ proximity} , ${compass} , ${barometer}</h6>
+        </div>
+         `
+    }
+    else {
     console.log (details.data)
     displayDetails.innerHTML = `
     <div class='p-5'>
@@ -48,9 +94,14 @@ const detailInfo = (details) => {
    </div>
    <h1 class='text-center'>${details.data.name}</h1>
    <h4 class='text-center'>${details.data.brand}</h4>
-   <h5 class='text-center'>${details.data.brand}</h5>
+   <h5 class='text-center'>${details.data.releaseDate}</h5>
+   <h6 class='text-center  mt-5'><span class='mx-3'>Bluetooth: ${Bluetooth}</span><span class='ms-3'>Gps: ${GPS}</span></h5>
+   <h6 class='text-center my-3'>Sensors : ${FaceID} , ${accelerometer} , ${gyro} , ${ proximity} , ${compass} , ${barometer}</h6>
+        <h6 class='text-center my-3'><span class='ms-5'>NFC: ${NFC}</span> <span class='ms-5'>Radio: ${Radio}</span><span class='ms-5'>Radio: ${USB}</span></h6>
+        <h6 class='text-center my-3'>WLAN: ${WLAN}</span></h6>
+        
    </div>
     `
 
-
+    }
 }
